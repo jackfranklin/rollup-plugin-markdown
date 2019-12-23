e4236ea142c38d90f86ba9c3cd079ce4e3be56fd
Jack Franklin <jack@jackfranklin.net>
2019-12-23 02:16:57 +0000

    first version from project

---
+++ src/rollup-plugin-markdown.js
@@ -0,0 +1,39 @@
+const { createFilter } = require('rollup-pluginutils')
+const path = require('path')
+const matter = require('gray-matter')
+const showdown = require('showdown')
+
+const converter = new showdown.Converter({
+  metadata: true,
+})
+
+converter.setFlavor('github')
+
+export default function markdownPlugin(options = {}) {
+  const filter = createFilter(options.include, options.exclude)
+
+  return {
+    name: 'rollup-plugin-markdown',
+    transform(code, id) {
+      if (!filter(id) === -1) return
+
+      const extension = path.extname(id)
+
+      if (extension !== '.md') return
+
+      const matterResult = matter(code)
+      const html = converter.makeHtml(matterResult.content)
+
+      const exportFromModule = JSON.stringify({
+        html,
+        metadata: matterResult.data,
+        filename: path.basename(id),
+      })
+
+      return {
+        code: `export default ${exportFromModule}`,
+        map: { mappings: '' },
+      }
+    },
+  }
+}
